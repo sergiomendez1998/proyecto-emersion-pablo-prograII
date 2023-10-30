172b9bc6963444e1f02b297658524558736384cf
sergiomendez2 <sergio.mendez2@telusinternational.com>
2023-10-29 18:36:17 -0600

    initial commit

---
+++ src/main/java/com/example/proyectofinalprograiimvc/ProyectoFinalPrograIiMvcApplication.java
@@ -0,0 +1,13 @@
+package com.example.proyectofinalprograiimvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFinalPrograIiMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoFinalPrograIiMvcApplication.class, args);
+	}
+
+}
