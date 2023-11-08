0edc52b241752e9fb9e6e64362ea8d73df0b4eec
pablo <pablochavez9817@gmail.com>
2023-11-07 22:11:45 -0600

    agregando la cache

--- src/main/java/com/example/proyectofinalprograiimvc/controllers/HomeController.java
+++ src/main/java/com/example/proyectofinalprograiimvc/controllers/HomeController.java
@@ -19,6 +19,8 @@ public class HomeController {
     @Autowired
     private RolServiceImpl rolService;
 
+    
+
     @GetMapping("/")
     public String Login(){
         log.info("Ejecutando el controlador Spring MVC");
