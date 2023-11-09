07d4b9f010a928871459f161b172d7eda0614ea8
DannyGalvan <cgalvan29111999@gmail.com>
2023-11-09 02:22:09 -0600

    code refactor

--- src/main/resources/static/JS/catalogs.js
+++ src/main/resources/static/JS/catalogs.js
@@ -1,14 +1,42 @@
 const TABLE_VALUES = [
-    "table_items" ,
-    "table_items2" ,
-    "table_items3" ,
-    "table_items4" ,
-    "table_items5" ,
-    "table_items6"
+    {
+        id: "article_0",
+        name: "--Seleccione un Catálogo--",
+        selected: false,
+    },
+    {
+        id: "article_table_items",
+        name: "Cátalogo de Items",
+        selected: true,
+    },
+    {
+        id: "article_table_items2",
+        name: "Cátalogo de Roles",
+        selected: false,
+    },
+    {
+        id: "article_table_items3",
+        name: "Cátalogo de Estados",
+        selected: false,
+    },
+    {
+        id: "article_table_items4",
+        name: "Cátalogo de Examenes",
+        selected: false,
+    },
+    {
+        id: "article_table_items5",
+        name: "Cátalogo de Muestras",
+        selected: false,
+    },
+    {
+        id: "article_table_items6",
+        name: "Cátalogo de Unidades de Medida",
+        selected: false,
+    },
 ]
 
-
-new Tabulator("#table_items", {
+const CONFIG_TABLE = {
     layout: "fitColumns",
     responsiveLayout: "collapse",
     columns: [
@@ -16,86 +44,45 @@ new Tabulator("#table_items", {
         { title: "ID", hozAlign: "left", minWidth: 50, width: 75 },
         { title: "NOMBRE", hozAlign: "center", minWidth: 50 },
         { title: "DESCRIPCIÓN", hozAlign: "center", minWidth: 300 },
-        { title: "FECHA", hozAlign: "right", minWidth: 300 },
-        { title: "FECHA ACTUAL", hozAlign: "right", minWidth: 300 },
+        { title: "FECHA CREACIÓN", hozAlign: "right", minWidth: 300 },
+        { title: "FECHA ACTUALIZACIÓN", hozAlign: "right", minWidth: 300 },
     ]
-});
+};
 
-new Tabulator("#table_items2", {
-    layout: "fitColumns",
-    responsiveLayout: "collapse",
-    columns: [
-        { formatter: "responsiveCollapse", width: 30, minWidth: 30, hozAlign: "center", resizable: false, headerSort: false },
-        { title: "ID", hozAlign: "left", minWidth: 50, width: 75 },
-        { title: "NOMBRE", hozAlign: "center", minWidth: 50 },
-        { title: "DESCRIPCIÓN", hozAlign: "center", minWidth: 300 },
-        { title: "FECHA", hozAlign: "right", minWidth: 300 },
-        { title: "FECHA ACTUAL", hozAlign: "right", minWidth: 300 },
-    ]
-});
 
-new Tabulator("#table_items3", {
-    layout: "fitColumns",
-    responsiveLayout: "collapse",
-    columns: [
-        { formatter: "responsiveCollapse", width: 30, minWidth: 30, hozAlign: "center", resizable: false, headerSort: false },
-        { title: "ID", hozAlign: "left", minWidth: 50, width: 75 },
-        { title: "NOMBRE", hozAlign: "center", minWidth: 50 },
-        { title: "DESCRIPCIÓN", hozAlign: "center", minWidth: 300 },
-        { title: "FECHA", hozAlign: "right", minWidth: 300 },
-        { title: "FECHA ACTUAL", hozAlign: "right", minWidth: 300 },
-    ]
-});
+document.addEventListener("DOMContentLoaded", ()=> {
+    const select = document.querySelector("#select_Catalogos");
 
-new Tabulator("#table_items4", {
-    layout: "fitColumns",
-    responsiveLayout: "collapse",
-    columns: [
-        { formatter: "responsiveCollapse", width: 30, minWidth: 30, hozAlign: "center", resizable: false, headerSort: false },
-        { title: "ID", hozAlign: "left", minWidth: 50, width: 75 },
-        { title: "NOMBRE", hozAlign: "center", minWidth: 50 },
-        { title: "DESCRIPCIÓN", hozAlign: "center", minWidth: 300 },
-        { title: "FECHA", hozAlign: "right", minWidth: 300 },
-        { title: "FECHA MODIFICADA", hozAlign: "right", minWidth: 300 },
-    ]
-});
+    const select_items = TABLE_VALUES.map(t=>{
+        const option = document.createElement("option");
+        option.value = t.id;
+        option.textContent = t.name;
+        option.selected = t.selected;
+        return option;
+    });
 
-new Tabulator("#table_items5", {
-    layout: "fitColumns",
-    responsiveLayout: "collapse",
-    columns: [
-        { formatter: "responsiveCollapse", width: 30, minWidth: 30, hozAlign: "center", resizable: false, headerSort: false },
-        { title: "ID", hozAlign: "left", minWidth: 50, width: 75 },
-        { title: "NOMBRE", hozAlign: "center", minWidth: 50 },
-        { title: "DESCRIPCIÓN", hozAlign: "center", minWidth: 300 },
-        { title: "FECHA", hozAlign: "right", minWidth: 300 },
-        { title: "FECHA ACTUAL", hozAlign: "right", minWidth: 300 },
-    ]
-});
+    select.append(...select_items);
 
-new Tabulator("#table_items6", {
-    layout: "fitColumns",
-    responsiveLayout: "collapse",
-    columns: [
-        { formatter: "responsiveCollapse", width: 30, minWidth: 30, hozAlign: "center", resizable: false, headerSort: false },
-        { title: "ID", hozAlign: "left", minWidth: 50, width: 75 },
-        { title: "NOMBRE", hozAlign: "center", minWidth: 50 },
-        { title: "DESCRIPCIÓN", hozAlign: "center", minWidth: 300 },
-        { title: "FECHA", hozAlign: "right", minWidth: 300 },
-        { title: "FECHA MODIFICADA", hozAlign: "right", minWidth: 300 },
-    ]
-});
+    new Tabulator("#table_items", CONFIG_TABLE);
 
-const select = document.querySelector("#select_Catalogos") ;
+    new Tabulator("#table_items2", CONFIG_TABLE);
 
-select.addEventListener("input", (e)=>{
-    const selected = e.target.value;
-    const table = document.querySelector(`#${selected}`);
-    table.classList.remove("d-none");
-    const filterValues = TABLE_VALUES.filter(t=> t!== selected);
-    filterValues.forEach(t=>{
-        const table = document.querySelector(`#${t}`);
-        table.classList.add("d-none");
+    new Tabulator("#table_items3", CONFIG_TABLE);
+
+    new Tabulator("#table_items4", CONFIG_TABLE);
+
+    new Tabulator("#table_items5", CONFIG_TABLE);
+
+    new Tabulator("#table_items6", CONFIG_TABLE);
+
+    select.addEventListener("input", (e)=>{
+        const selected = e.target.value;
+        const table = document.querySelector(`#${selected}`) || null;
+        table != null && table.classList.remove("d-none");
+
+        TABLE_VALUES.filter(t=> t.id !== selected).forEach(t=>{
+            const table = document.querySelector(`#${t.id}`) || null;
+            table != null && table.classList.add("d-none");
+        });
     })
-
-})
+});
