80d542ee067ad3b94b825f64a5b7de75bc18db17
Jackeline Sanchez <sanchezjackeline977@gmail.com>
2023-11-09 01:34:31 -0600

    FRONTEND UPDATE 3.1

--- src/main/resources/static/JS/catalogs.js
+++ src/main/resources/static/JS/catalogs.js
@@ -1,4 +1,14 @@
-var table = new Tabulator("#table_items", {
+const TABLE_VALUES = [
+    "table_items" ,
+    "table_items2" ,
+    "table_items3" ,
+    "table_items4" ,
+    "table_items5" ,
+    "table_items6"
+]
+
+
+new Tabulator("#table_items", {
     layout: "fitColumns",
     responsiveLayout: "collapse",
     columns: [
@@ -11,7 +21,7 @@ var table = new Tabulator("#table_items", {
     ]
 });
 
-var table = new Tabulator("#table_items2", {
+new Tabulator("#table_items2", {
     layout: "fitColumns",
     responsiveLayout: "collapse",
     columns: [
@@ -24,7 +34,7 @@ var table = new Tabulator("#table_items2", {
     ]
 });
 
-var table = new Tabulator("#table_items3", {
+new Tabulator("#table_items3", {
     layout: "fitColumns",
     responsiveLayout: "collapse",
     columns: [
@@ -37,7 +47,7 @@ var table = new Tabulator("#table_items3", {
     ]
 });
 
-var table = new Tabulator("#table_items4", {
+new Tabulator("#table_items4", {
     layout: "fitColumns",
     responsiveLayout: "collapse",
     columns: [
@@ -50,7 +60,7 @@ var table = new Tabulator("#table_items4", {
     ]
 });
 
-var table = new Tabulator("#table_items5", {
+new Tabulator("#table_items5", {
     layout: "fitColumns",
     responsiveLayout: "collapse",
     columns: [
@@ -62,3 +72,30 @@ var table = new Tabulator("#table_items5", {
         { title: "FECHA ACTUAL", hozAlign: "right", minWidth: 300 },
     ]
 });
+
+new Tabulator("#table_items6", {
+    layout: "fitColumns",
+    responsiveLayout: "collapse",
+    columns: [
+        { formatter: "responsiveCollapse", width: 30, minWidth: 30, hozAlign: "center", resizable: false, headerSort: false },
+        { title: "ID", hozAlign: "left", minWidth: 50, width: 75 },
+        { title: "NOMBRE", hozAlign: "center", minWidth: 50 },
+        { title: "DESCRIPCIÓN", hozAlign: "center", minWidth: 300 },
+        { title: "FECHA", hozAlign: "right", minWidth: 300 },
+        { title: "FECHA MODIFICADA", hozAlign: "right", minWidth: 300 },
+    ]
+});
+
+const select = document.querySelector("#select_Catalogos") ;
+
+select.addEventListener("input", (e)=>{
+    const selected = e.target.value;
+    const table = document.querySelector(`#${selected}`);
+    table.classList.remove("d-none");
+    const filterValues = TABLE_VALUES.filter(t=> t!== selected);
+    filterValues.forEach(t=>{
+        const table = document.querySelector(`#${t}`);
+        table.classList.add("d-none");
+    })
+
+})
