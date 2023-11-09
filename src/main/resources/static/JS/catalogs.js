cf0230c395b6eebcc64522d3446f7984b5772b3a
Jackeline Sanchez <sanchezjackeline977@gmail.com>
2023-11-08 20:06:33 -0600

    FRONTEND UPDATE 3

--- src/main/resources/static/JS/catalogs.js
+++ src/main/resources/static/JS/catalogs.js
@@ -10,3 +10,55 @@ var table = new Tabulator("#table_items", {
         { title: "FECHA ACTUAL", hozAlign: "right", minWidth: 300 },
     ]
 });
+
+var table = new Tabulator("#table_items2", {
+    layout: "fitColumns",
+    responsiveLayout: "collapse",
+    columns: [
+        { formatter: "responsiveCollapse", width: 30, minWidth: 30, hozAlign: "center", resizable: false, headerSort: false },
+        { title: "ID", hozAlign: "left", minWidth: 50, width: 75 },
+        { title: "NOMBRE", hozAlign: "center", minWidth: 50 },
+        { title: "DESCRIPCIÓN", hozAlign: "center", minWidth: 300 },
+        { title: "FECHA", hozAlign: "right", minWidth: 300 },
+        { title: "FECHA ACTUAL", hozAlign: "right", minWidth: 300 },
+    ]
+});
+
+var table = new Tabulator("#table_items3", {
+    layout: "fitColumns",
+    responsiveLayout: "collapse",
+    columns: [
+        { formatter: "responsiveCollapse", width: 30, minWidth: 30, hozAlign: "center", resizable: false, headerSort: false },
+        { title: "ID", hozAlign: "left", minWidth: 50, width: 75 },
+        { title: "NOMBRE", hozAlign: "center", minWidth: 50 },
+        { title: "DESCRIPCIÓN", hozAlign: "center", minWidth: 300 },
+        { title: "FECHA", hozAlign: "right", minWidth: 300 },
+        { title: "FECHA ACTUAL", hozAlign: "right", minWidth: 300 },
+    ]
+});
+
+var table = new Tabulator("#table_items4", {
+    layout: "fitColumns",
+    responsiveLayout: "collapse",
+    columns: [
+        { formatter: "responsiveCollapse", width: 30, minWidth: 30, hozAlign: "center", resizable: false, headerSort: false },
+        { title: "ID", hozAlign: "left", minWidth: 50, width: 75 },
+        { title: "NOMBRE", hozAlign: "center", minWidth: 50 },
+        { title: "DESCRIPCIÓN", hozAlign: "center", minWidth: 300 },
+        { title: "FECHA", hozAlign: "right", minWidth: 300 },
+        { title: "FECHA MODIFICADA", hozAlign: "right", minWidth: 300 },
+    ]
+});
+
+var table = new Tabulator("#table_items5", {
+    layout: "fitColumns",
+    responsiveLayout: "collapse",
+    columns: [
+        { formatter: "responsiveCollapse", width: 30, minWidth: 30, hozAlign: "center", resizable: false, headerSort: false },
+        { title: "ID", hozAlign: "left", minWidth: 50, width: 75 },
+        { title: "NOMBRE", hozAlign: "center", minWidth: 50 },
+        { title: "DESCRIPCIÓN", hozAlign: "center", minWidth: 300 },
+        { title: "FECHA", hozAlign: "right", minWidth: 300 },
+        { title: "FECHA ACTUAL", hozAlign: "right", minWidth: 300 },
+    ]
+});
